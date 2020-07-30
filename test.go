463ccb8a4fac7eecbd0e2e0eb7ca22e041821fe8
Ubuntu <ubuntu@ip-172-31-0-27.cn-northwest-1.compute.internal>
2020-07-30 10:19:50 +0000

    modify2

---
+++ test.go
@@ -0,0 +1,151 @@
+// s3-benchmark.go
+// Copyright (c) 2017 Wasabi Technology, Inc.
+
+package main
+
+import (
+//	"bytes"
+//	"crypto/hmac"
+	"crypto/md5"
+//	"crypto/sha1"
+	"crypto/tls"
+	"encoding/base64"
+	"flag"
+	"fmt"
+	"code.cloudfoundry.org/bytefmt"
+	"github.com/aws/aws-sdk-go/aws"
+//	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+//	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+//	"github.com/pivotal-golang/bytefmt"
+//	"io"
+//	"io/ioutil"
+	"log"
+	"math/rand"
+	"net"
+	"net/http"
+	"os"
+//	"sort"
+//	"strconv"
+//	"strings"
+//	"sync"
+//	"sync/atomic"
+	"time"
+)
+
+// Global variables
+//var access_key, secret_key, url_host, bucket, region string
+var bucket string
+var duration_secs, threads, loops int
+var object_size uint64
+var object_data []byte
+var object_data_md5 string
+var running_threads, upload_count, download_count, delete_count, upload_slowdown_count, download_slowdown_count, delete_slowdown_count int32
+var endtime, upload_finish, download_finish, delete_finish time.Time
+
+func logit(msg string) {
+	fmt.Println(msg)
+	logfile, _ := os.OpenFile("benchmark.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+	if logfile != nil {
+		logfile.WriteString(time.Now().Format(http.TimeFormat) + ": " + msg + "\n")
+		logfile.Close()
+	}
+}
+
+// Our HTTP transport used for the roundtripper below
+var HTTPTransport http.RoundTripper = &http.Transport{
+	Proxy: http.ProxyFromEnvironment,
+	Dial: (&net.Dialer{
+		Timeout:   30 * time.Second,
+		KeepAlive: 30 * time.Second,
+	}).Dial,
+	TLSHandshakeTimeout:   10 * time.Second,
+	ExpectContinueTimeout: 0,
+	// Allow an unlimited number of idle connections
+	MaxIdleConnsPerHost: 4096,
+	MaxIdleConns:        0,
+	// But limit their idle time
+	IdleConnTimeout: time.Minute,
+	// Ignore TLS errors
+	TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+}
+
+var httpClient = &http.Client{Transport: HTTPTransport}
+
+func getS3Client() *s3.S3 {
+	// Build our config
+//	creds := credentials.NewStaticCredentials(access_key, secret_key, "")
+	loglevel := aws.LogOff
+	// Build the rest of the configuration
+	awsConfig := &aws.Config{
+		Region:               aws.String("cn-northwest-1"),
+		LogLevel:             &loglevel,
+		S3ForcePathStyle:     aws.Bool(true),
+		S3Disable100Continue: aws.Bool(true),
+		// Comment following to use default transport
+		HTTPClient: &http.Client{Transport: HTTPTransport},
+	}
+	session := session.New(awsConfig)
+	client := s3.New(session)
+	if client == nil {
+		log.Fatalf("FATAL: Unable to create new client.")
+	}
+	// Return success
+	return client
+}
+
+func createBucket(ignore_errors bool) {
+	// Get a client
+	client := getS3Client()
+	// Create our bucket (may already exist without error)
+	in := &s3.CreateBucketInput{Bucket: aws.String(bucket)}
+	if _, err := client.CreateBucket(in); err != nil {
+		if ignore_errors {
+			log.Printf("WARNING: createBucket %s error, ignoring %v", bucket, err)
+		} else {
+			log.Fatalf("FATAL: Unable to create bucket %s (is your access and secret correct?): %v", bucket, err)
+		}
+	}
+}
+
+
+func main() {
+	// Hello
+
+	// Parse command line
+	myflag := flag.NewFlagSet("myflag", flag.ExitOnError)
+	myflag.StringVar(&bucket, "b", "zilliz-hz01", "Bucket for testing")
+	myflag.IntVar(&duration_secs, "d", 60, "Duration of each test in seconds")
+	myflag.IntVar(&threads, "t", 1, "Number of threads to run")
+	myflag.IntVar(&loops, "l", 1, "Number of times to repeat test")
+	var sizeArg string
+	myflag.StringVar(&sizeArg, "z", "1M", "Size of objects in bytes with postfix K, M, and G")
+	if err := myflag.Parse(os.Args[1:]); err != nil {
+		os.Exit(1)
+	}
+
+	// Check the arguments
+	var err error
+	if object_size, err = bytefmt.ToBytes(sizeArg); err != nil {
+		log.Fatalf("Invalid -z argument for object size: %v", err)
+	}
+
+	logit(fmt.Sprintf("Parameters: bucket=%s, duration=%d, threads=%d, loops=%d, size=%s",
+		bucket, duration_secs, threads, loops, sizeArg))
+
+	// Initialize data for the bucket
+	object_data = make([]byte, object_size)
+	rand.Read(object_data)
+	hasher := md5.New()
+	hasher.Write(object_data)
+	object_data_md5 = base64.StdEncoding.EncodeToString(hasher.Sum(nil))
+
+	// Create the bucket and delete all the objects
+	fmt.Println("Start to create Bucket!")
+	createBucket(true)
+	fmt.Println("Create Bucket Done!")
+	// Loop running the tests
+
+	// All done
+}
